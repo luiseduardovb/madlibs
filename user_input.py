53800cd1e68f6410bb93d08742d792fcee6c720d
luiseduardovb <66119658+luiseduardovb@users.noreply.github.com>
2022-04-09 10:52:26 +0300

    Add user input functions

---
+++ user_input.py
@@ -0,0 +1,20 @@
+def get_time():
+    time = input("Enter a number from 1 to 12: ")
+    return time
+
+def get_noun():
+    noun = input("Enter a noun (plural): ")
+    return noun
+
+def get_name():
+    name = input("Enter a name: ")
+    return name.title()
+
+def get_sentence():
+    sentence = input("Enter any sentence: ")
+    return sentence.upper()
+
+def get_verb():
+    verb = input("Enter a verb: ")
+    return verb
+
